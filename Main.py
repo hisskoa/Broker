eb8078d711fbabe773bfdf4901b6cad9ebe0b865
hisskoa <96256575+hisskoa@users.noreply.github.com>
2024-11-15 19:05:53 +0300

    change

--- Main.py
+++ Main.py
@@ -1,21 +1,41 @@
-import queue
-from Producer import producer
-from Consumer import consumer
-
+class My_Stack:                 # LIFO — Last In First Out («последним пришел, первым ушел»).
+    def __init__(self):
+        self.stack = []
+        
+    def push(self, item):          # Добавить элемент на вершину стека
+        self.stack.append(item)
+        
+    def pop(self):              # Достать самый верхний элемент стека
+        return self.stack.pop()
+        
+class Queue:                    # FIFO - First In First Out («первым пришёл — первым ушёл»)
+    def __init__(self):
+        self.s1 = My_Stack()
+        self.s2 = My_Stack()
+        
+    def push(self, x):          # Добавить элемент в конец очереди
+        self.s1.push(x)
+        
+    def pop(self):              # Достать элемент из начала очереди
+        if not self.s2.stack:
+            if not self.s1.stack:
+                print("Error!")
+            while self.s1.stack:
+                self.s2.push(self.s1.pop())
+        return self.s2.pop() if self.s2.stack else None
+    
 class Broker:
-
     def __init__(self):
         self.broker = {}
         
-    def put(self, topic, message):
+    def put(self, topic, message):      # Добавить в брокера
         if topic not in self.broker:
-            self.broker[topic] = queue.Queue()
-            
-        producer(self.broker, topic, message)    
+            self.broker[topic] = Queue()              
+        return self.broker[topic].push(message)     
                     
-    def consume(self, topic): 
-        return consumer(self.broker, topic)
-        
+    def consume(self, topic):           # Достать из брокера       
+        return self.broker[topic].pop()            
+
 broker = Broker()
 
 while(True):
